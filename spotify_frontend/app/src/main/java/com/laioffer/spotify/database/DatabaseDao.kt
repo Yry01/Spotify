26ad9194ae18b806971ded23052ab7f4bd4ac11f
Yry01 <103943433+Yry01@users.noreply.github.com>
2024-10-11 00:06:03 -0400

    Integrate with ExoPlayer for playing music

--- spotify_frontend/app/src/main/java/com/laioffer/spotify/database/DatabaseDao.kt
+++ spotify_frontend/app/src/main/java/com/laioffer/spotify/database/DatabaseDao.kt
@@ -10,9 +10,12 @@ interface DatabaseDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun favoriteAlbum(album: Album)
 
-    @Query("SELECT EXISTS(SELECT * FROM Album WHERE id = :id)")
+    @Query("SELECT  EXISTS(SELECT * FROM Album WHERE id = :id)")
     fun isFavoriteAlbum(id : Int) : Flow<Boolean>
 
     @Delete
     fun unFavoriteAlbum(album: Album)
+
+    @Query("select * from Album")
+    fun fetchFavoriteAlbums(): Flow<List<Album>>
 }
--- spotify_frontend/app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistFragment.kt
+++ spotify_frontend/app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistFragment.kt
@@ -9,14 +9,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.laioffer.spotify.player.PlayerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PlaylistFragment : Fragment() {
     private val navArgs by navArgs<PlaylistFragmentArgs>()
     private val viewModel: PlaylistViewModel by viewModels()
+    private val playerViewModel: PlayerViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -28,7 +31,8 @@ class PlaylistFragment : Fragment() {
             setContent {
                 MaterialTheme(colors = darkColors()) {
                     PlaylistScreen(
-                        playlistViewModel = viewModel
+                        playlistViewModel = viewModel,
+                        playerViewModel = playerViewModel
                     )
                 }
             }
